9bca8c46a184a2bc9daf257208cdad4a68580391
chaptone <rakkan.j@gmail.com>
2021-01-19 11:50:58 +0700

    Simple typescript google map api integreate

---
+++ src/Map.ts
@@ -0,0 +1,40 @@
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class Map {
+  private googleMap: google.maps.Map;
+
+  constructor(devId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(devId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      console.log("map");
+
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap);
+    });
+  }
+}
